46714d69b791f314134a1a1ef6b91f875180a0c1
Timothy Wong <timothy300077@gmail.com>
2024-07-15 01:11:21 +0800

    -update share function test 2

--- scripts/share_button.js
+++ scripts/share_button.js
@@ -1,7 +1,43 @@
-function copyPageUrl() {
-    navigator.clipboard.writeText(window.location.href).then(function() {
-      alert('Link copied to clipboard!');
-    }, function(err) {
-      console.error('Could not copy text: ', err);
-    });
-  }
+// Sharing functions
+function shareToWhatsApp() {
+  var url = encodeURIComponent(window.location.href);
+  var text = encodeURIComponent("Check out this page: ");
+  window.open(`https://wa.me/?text=${text}${url}`, '_blank');
+}
+
+function shareToFacebook() {
+  var url = encodeURIComponent(window.location.href);
+  window.open(`https://www.facebook.com/sharer/sharer.php?u=${url}`, '_blank');
+}
+
+function shareToTwitter() {
+  var url = encodeURIComponent(window.location.href);
+  var text = encodeURIComponent("Check out this page: ");
+  window.open(`https://twitter.com/intent/tweet?url=${url}&text=${text}`, '_blank');
+}
+
+function shareToLinkedIn() {
+  var url = encodeURIComponent(window.location.href);
+  window.open(`https://www.linkedin.com/sharing/share-offsite/?url=${url}`, '_blank');
+}
+
+function shareToInstagram() {
+    var url = encodeURIComponent(window.location.href);
+    var instagramUrl = `instagram://story-camera?url=${url}`;
+    
+    // Try to open Instagram app
+    window.location.href = instagramUrl;
+    
+    // Fallback to website if app doesn't open after a short delay
+    setTimeout(function() {
+        window.location.href = 'https://www.instagram.com/';
+    }, 2000);
+}
+
+function copyLink() {
+  navigator.clipboard.writeText(window.location.href).then(function() {
+    alert('Link copied to clipboard!');
+  }, function(err) {
+    console.error('Could not copy text: ', err);
+  });
+}
